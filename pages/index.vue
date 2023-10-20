e8777f2b11fc7b34a77026b2411c26b5a1687696
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-10-20 10:55:45 -0300

    Começando a pagina principal

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div class="grid grid-rows-3 grid-flow-col gap-1">
+            <div class="row-span-3 bg-black w-96 h-96">01</div>
+            <div class="col-span-2 bg-black">02</div>
+            <div class="row-span-2 col-span-2 bg-black">03</div>
+
+        </div>
+    </div>
+</template>
