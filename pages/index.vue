ae9fd041b145456a1d651e0446138986de884087
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-10-23 11:13:01 -0300

    Pagina index

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,23 @@
 <template>
     <div>
-        <div class="grid grid-rows-3 grid-flow-col gap-1">
-            <div class="row-span-3 bg-black w-96 h-96">01</div>
-            <div class="col-span-2 bg-black">02</div>
-            <div class="row-span-2 col-span-2 bg-black">03</div>
-
+      <div class="grid grid-rows-6 grid-flow-col h-96 gap-4">
+        <div class="row-span-6 bg-gray-100 rounded-xl">
+          <img src="/perfume.jpg" alt="PERFUME" class="rounded-2xl object-cover h-full w-full">
         </div>
+
+        <!-- SEGUNDA IMAGEM -->
+        <div class="row-span-2 col-span-11 bg-gray-100">
+            <!-- <img src="/perfumes.jpg" alt="perfumes" class="h-auto"> -->
+        </div>
+        <div class="row-span-2 col-span-11 bg-gray-100" style="position: relative;">
+        </div>
+        <div class="row-span-2 col-span-8 bg-gray-100">
+          <!-- Outra parte do layout -->
+        </div>
+        <div class="row-span-2 col-span-3 bg-gray-100">
+          <!-- Outra parte do layout -->
+        </div>
+      </div>
     </div>
-</template>
+  </template>
+  
