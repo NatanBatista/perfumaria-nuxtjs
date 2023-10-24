0fb767fdc2579c9d28e7962d9385fac1c17aaefc
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-10-24 08:58:55 -0300

    Criando componente homeFotos para home

---
+++ components/home/homeFotos.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+    <div class="grid grid-rows-6 grid-cols-10 grid-flow-col h-96 gap-4">
+      <div class="row-span-6 col-span-4 rounded-xl transition-transform hover:scale-110">
+        <div class="relative h-full">
+          <img 
+            src="/perfume.jpg"
+            @click="modalPerfume = true"
+            alt="PERFUME"
+            class="rounded-2xl object-cover w-full h-full">
+        </div>
+      </div>
+
+      <!-- SEGUNDA IMAGEM -->
+      <div class="row-span-4 col-span-6 transition-transform hover:scale-110">
+        <div class="h-full">
+          <img 
+            src="/perfumes.jpg" 
+            @click="modalPerfumes = true"
+            alt="perfumes" 
+            class="rounded-2xl object-cover w-full h-full">
+        </div>
+      </div>
+      <div class="row-span-2 col-span-6">
+        <img 
+          src="/perfumes2.jpg"
+          @click="modalPerfumes2 = true"
+          alt="perfumes"
+           class="rounded-2xl object-cover w-full h-full">
+      </div>
+    </div>
+  </div>
+  <UModal v-model="modalPerfume">
+    <img src="/perfume.jpg" 
+      alt="perfumes" 
+      class="rounded-2xl">
+  </UModal>
+  <UModal v-model="modalPerfumes">
+    <img src="/perfumes.jpg" 
+      alt="perfumes" 
+      class="rounded-2xl">
+  </UModal>
+  <UModal v-model="modalPerfumes2">
+    <img src="/perfumes2.jpg" 
+      alt="perfumes" 
+      class="rounded-2xl">
+  </UModal>
+</template>
+
+<script setup>
+const modalPerfume = ref(false)
+const modalPerfumes = ref(false)
+const modalPerfumes2 = ref(false)
+</script>
