71e01f5804c8ff1c026a8f7f36f3cb87b82d686e
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-10-25 21:39:00 -0300

    style: Adicionando Logo a navbar

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -1,61 +1,68 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="my-6">
-        <nav class="flex justify-between items-center">
-            <div class="flex gap-10 items-center">
-                <UIcon @click="slideover = true" name="i-heroicons-bars-3" class="w-10 h-10" />
-                <span class=" font-bold text-2xl tracking-tight"> Perfumaria </span>
-            </div>
-
-            <!-- Menu user -->
-            <div class="flex items-center gap-10">
-              <NavbarColorMode></NavbarColorMode>
-              <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }" :popper="{ placement: 'bottom-start' }">
-    <UIcon name="i-heroicons-user-circle" class="w-8 h-8"/>
-
-    <template #account="{ item }">
-      <div class="text-left">
-        <p>
-          Logado como
-        </p>
-        <p class="truncate font-medium text-gray-900 dark:text-white">
-          {{ item.label }}
-        </p>
+  <div class="my-6">
+    <nav class="flex justify-between items-center">
+      <div class="flex gap-10 items-center p-3">
+        <UIcon @click="slideover = true" name="i-heroicons-bars-3" class="w-10 h-10" />
+        <NuxtLink to="/">
+          <img class=" w-12 h-12 rounded-full cursor-pointer" src="/logo.jpg">
+        </NuxtLink>
+        <span class=" font-bold text-2xl tracking-tight"> Perfumaria </span>
       </div>
-    </template>
 
-    <template #item="{ item }">
-      <span class="truncate">{{ item.label }}</span>
+      <!-- Menu user -->
+      <div class="flex items-center gap-10">
+        <NavbarColorMode></NavbarColorMode>
+        <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }"
+          :popper="{ placement: 'bottom-start' }">
+          <UIcon name="i-heroicons-user-circle" class="w-8 h-8" />
 
-      <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
-    </template>
-  </UDropdown>
+          <template #account="{ item }">
+            <div class="text-left">
+              <p>
+                Logado como
+              </p>
+              <p class="truncate font-medium text-gray-900 dark:text-white">
+                {{ item.label }}
+              </p>
             </div>
-        </nav>
-    </div>
+          </template>
 
-    <USlideover v-model="slideover" side="left">
-      <UCard class="flex flex-col flex-1" :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
-        <template #header>
-          <div class="flex justify-between items-center">
-            <span> PERFUMARIA </span>
-            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="slideover = false" />
-          </div>
-        </template>
-          <ul class="flex flex-col gap-3 ml-5">
-            <NuxtLink to="/oi" class="hover:text-gray-500"> Perfumes </NuxtLink>
-            <NuxtLink to="/noticias" class="hover:text-gray-500"> Notícias </NuxtLink>
-            <NuxtLink to="sobre-nos" class="hover:text-gray-500"> Sobre-nós </NuxtLink>
-          </ul>
-        <template #footer>
-          <!-- Content -->
-        </template>
-      </UCard>
-    </USlideover>
+          <template #item="{ item }">
+            <span class="truncate">{{ item.label }}</span>
+
+            <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
+          </template>
+        </UDropdown>
+      </div>
+    </nav>
+    <div class="bg-[#333] h-1"></div>
+  </div>
+
+  <USlideover v-model="slideover" side="left">
+    <UCard class="flex flex-col flex-1"
+      :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+      <template #header>
+        <div class="flex justify-between items-center">
+          <span> PERFUMARIA </span>
+          <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+            @click="slideover = false" />
+        </div>
+      </template>
+      <ul class="flex flex-col gap-3 ml-5">
+        <NuxtLink to="/oi" class="hover:text-gray-500"> Perfumes </NuxtLink>
+        <NuxtLink to="/noticias" class="hover:text-gray-500"> Notícias </NuxtLink>
+        <NuxtLink to="sobre-nos" class="hover:text-gray-500"> Sobre-nós </NuxtLink>
+      </ul>
+      <template #footer>
+        <!-- Content -->
+      </template>
+    </UCard>
+  </USlideover>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue"
+import { ref } from "vue"
 const items = [
 	[{
 		label: "natan@example.com",
