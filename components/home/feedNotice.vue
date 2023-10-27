0feb997c7f4fdafe48f9873656835765eb36521c
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-10-27 09:57:00 -0300

    style: Adicionando Skeleton

--- components/home/feedNotice.vue
+++ components/home/feedNotice.vue
@@ -1,17 +1,33 @@
 <template>
-    <div class="m-10 ">
-        <div class="flex gap-6 items-center mb-4">
-            <UAvatar class="transition duration-700 ease-in-out hover:scale-150" size="md" alt="Avatar"
-                icon="i-heroicons-photo" />
-            <h2>
-                Natan Batista
-            </h2>
-        </div>
-        <span>
-            "At vero eos et accusamus et iusto odio dignissimos
-            ducimus qui blanditiis praesentium voluptatum deleniti
-            atque corrupti quos dolores et quas molestias excepturi
-            sint occaecati cupiditate non provident.
-        </span>
+    <div v-if="!isLoading" class="m-10">
+      <div class="flex gap-6 items-center mt-16 mb-4">
+        <UAvatar class="transition duration-700 ease-in-out hover:scale-150" size="md" alt="Avatar" icon="i-heroicons-photo" />
+        <h2>Natan Batista</h2>
+      </div>
+      <span>
+        "At vero eos et accusamus et iusto odio dignissimos
+        ducimus qui blanditiis praesentium voluptatum deleniti
+        atque corrupti quos dolores et quas molestias excepturi
+        sint occaecati cupiditate non provident.
+      </span>
     </div>
-</template>
+  
+    <div v-else class="m-10">
+      <div class="flex gap-6 items-center mt-16 mb-4">
+        <USkeleton class="h-10 w-10" :ui="{ rounded: 'rounded-full' }" />
+        <h2><USkeleton class="h-4 w-[150px]" /></h2>
+      </div>
+      <span>
+        <USkeleton v-for="item in 5" :key="item" class="h-4 w-[250px] mb-4" />
+      </span>
+    </div>
+  </template>
+  
+<script>
+export default {
+	props: {
+		isLoading: Boolean
+	}
+}
+</script>
+  
