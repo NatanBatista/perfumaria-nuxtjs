f092429c9de63a56068c7a6b905bb69ca1ec8df0
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-11-01 10:47:42 -0300

    style: Criando página de notícias

---
+++ components/notice/noticeFeed.vue
@@ -0,0 +1,11 @@
+<template>
+    <UContainer class="my-3">
+        <div class="lg:border border-b-2 rounded-2xl">
+            <img src="/perfume.jpg" class="p-1 w-full h-[452px] rounded-2xl object-cover">
+            <h1 class="font-bold text-lg m-6"> Um titulo bem feio e perfeito feito por ninguem</h1>
+            <div>
+                <h3 class="m-7"> Publicado por Natan Batista</h3>
+            </div>
+        </div>
+    </UContainer>
+</template>
---
+++ pages/noticias/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <UContainer class="">
+        <NoticeFeed></NoticeFeed>
+        <div class="flex justify-center flex-wrap gap-10 md:flex-nowrap">
+            <NoticeFeed></NoticeFeed>
+            <NoticeFeed></NoticeFeed>
+        </div>
+    </UContainer>
+</template>
+
+<script lang="ts">
+
+export default {
+	name: "NoticePage"
+}
+</script>
