757666e5b3b9353dc3cf61af7406d9df61c752ca
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-11-07 09:37:36 -0300

    style: Criando a rota index e show de noticias

---
+++ components/notice/noticeCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="border border-b-2 m-2">
+        <img src="/perfume.jpg" @click="modalImage = true" class="p-1 w-full h-96 rounded-2xl object-cover">
+        <NuxtLink :to="`/noticias/${id}`">
+            <h1 class="font-bold text-lg m-6"> Titulo monstruoso </h1>
+
+            <h3 class="p-6">At vero eos et accusamus et iusto odio dignissimos ducimus
+                qui blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas molestias excepturi sint occaecati cupiditate non provident,
+                similique sunt in culpa qui officia deserunt mollitia animi, id est laborum
+                et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+            </h3>
+
+            <div>
+                <h3 class="m-7"> Publicado por Natan Batista</h3>
+            </div>
+        </NuxtLink>
+    </div>
+
+    <UModal v-model="modalImage">
+        <img src="/perfume.jpg" />
+    </UModal>
+</template>
+
+<script lang="ts">
+export default {
+	name: "noticeCardPage",
+	props: {
+		id: Number
+	}
+}
+</script>
+
+<script setup lang="ts">
+import { ref } from "vue"
+const modalImage = ref(false)
+</script>
--- pages/noticias/index.vue
+++ pages/noticias/index.vue
@@ -5,6 +5,11 @@
             <NoticeFeed></NoticeFeed>
             <NoticeFeed></NoticeFeed>
         </div>
+        <div class="flex flex-col items-center gap-2">
+            <NoticeCard v-for="items in 5" :key="items" :id="items"></NoticeCard>
+
+            <UPagination v-model="page" :page-count="5" :total="items.length" />
+        </div>
     </UContainer>
 </template>
 
@@ -13,4 +18,10 @@
 export default {
 	name: "NoticePage"
 }
+</script>
+
+<script setup lang="ts">
+import {ref} from "vue"
+const page = ref(1)
+const items = ref(Array(55))
 </script>
