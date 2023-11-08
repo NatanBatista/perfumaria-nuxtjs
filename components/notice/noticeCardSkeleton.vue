5c2b47aa1aa1fa0fdb7eb1506200b856b7dcfd84
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-11-08 10:56:39 -0300

    style: Adicionando UContainer ao skeleton

--- components/notice/noticeCardSkeleton.vue
+++ components/notice/noticeCardSkeleton.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="border border-b-2 m-2">
-        <USkeleton class="p-1 w-full h-96 rounded-2xl" :ui=" { background: 'bg-gray-300 dark:bg-neutral-700'}"/>
-        <NuxtLink :to="`/noticias/${id}`">
-            <USkeleton class="m-6 w-32 h-4" :ui=" { background: 'bg-gray-300 dark:bg-neutral-700'}"/>
-            <USkeleton class="m-6 h-4 w-72" :ui=" { background: 'bg-gray-300 dark:bg-neutral-700'}"/>
+    <UContainer>
+        <div class="border border-b-2 m-2">
+            <USkeleton class="p-1 w-full h-96 rounded-2xl" :ui="{ background: 'bg-gray-300 dark:bg-neutral-700' }" />
+            <NuxtLink :to="`/noticias/${id}`">
+                <USkeleton class="m-6 w-32 h-4" :ui="{ background: 'bg-gray-300 dark:bg-neutral-700' }" />
+                <USkeleton class="m-6 h-4 w-72" :ui="{ background: 'bg-gray-300 dark:bg-neutral-700' }" />
 
-            <div>
-                <USkeleton class="m-7 h-4 w-44" :ui=" { background: 'bg-gray-300 dark:bg-neutral-700'}"/>
-            </div>
-        </NuxtLink>
-    </div>
+                <div>
+                    <USkeleton class="m-7 h-4 w-44" :ui="{ background: 'bg-gray-300 dark:bg-neutral-700' }" />
+                </div>
+            </NuxtLink>
+        </div>
+    </UContainer>
 
     <UModal v-model="modalImage">
         <img src="/perfume.jpg" />
