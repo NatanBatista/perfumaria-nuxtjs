124a3d6b29b13fa4b59f1c6701fa4ed508a109c8
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-11-08 10:56:50 -0300

    feat: Criando requisição GET em /articles

--- components/notice/noticeCard.vue
+++ components/notice/noticeCard.vue
@@ -1,37 +1,63 @@
 <template>
-    <div class="border border-b-2 m-2">
-        <img src="/perfume.jpg" @click="modalImage = true" class="p-1 w-full h-96 rounded-2xl object-cover">
-        <NuxtLink :to="`/noticias/${id}`">
-            <h1 class="font-bold text-lg m-6"> Titulo monstruoso </h1>
+    <div v-if="!isLoading">
+        <div v-for="item in responseData" :key="item.id" class="border border-b-2 m-2">
+            <NuxtLink :to="`/noticias/${item.id}`">
+                <img :src="$config.public.appUrl + item.image.url" @click="modalImage = true"
+                    class="p-1 w-full h-96 rounded-2xl object-cover">
+                <h1 class="font-bold text-lg m-6"> {{ item.title }} </h1>
 
-            <h3 class="p-6">At vero eos et accusamus et iusto odio dignissimos ducimus
-                qui blanditiis praesentium voluptatum deleniti atque corrupti quos
-                dolores et quas molestias excepturi sint occaecati cupiditate non provident,
-                similique sunt in culpa qui officia deserunt mollitia animi, id est laborum
-                et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-            </h3>
+                <h3 class="p-6">
+                    {{ item.description }}
+                </h3>
 
-            <div>
-                <h3 class="m-7"> Publicado por Natan Batista</h3>
-            </div>
-        </NuxtLink>
+                <div>
+                    <h3 class="m-7"> Publicado por Natan Batista</h3>
+                </div>
+            </NuxtLink>
+        </div>
     </div>
 
-    <UModal v-model="modalImage">
-        <img src="/perfume.jpg" />
-    </UModal>
+    <NoticeCardSkeleton v-else />
 </template>
 
 <script lang="ts">
 export default {
 	name: "noticeCardPage",
 	props: {
-		id: Number
+		id: Number,
 	}
+}
+interface Article {
+    id: number
+    title: string
+    description: string
+    image: {
+        url: string
+    }
 }
 </script>
 
 <script setup lang="ts">
+
 import { ref } from "vue"
+import axios from "axios"
 const modalImage = ref(false)
+
+// eslint-disable-next-line no-undef
+const config = useRuntimeConfig()
+
+const responseData = ref(<Article[]>[])
+const isLoading = ref(true)
+
+const fetchData = async () => {
+	try {
+		const response = await axios.get(config.public.appUrl + "/articles")
+		responseData.value = response.data
+		console.log("DADOS: ", responseData.value)
+		isLoading.value = false
+	} catch (error) {
+		console.error("Erro ao buscar os artigos: ", error)
+	}
+}
+fetchData()
 </script>
--- pages/noticias/index.vue
+++ pages/noticias/index.vue
@@ -1,13 +1,15 @@
 <template>
     <UContainer class="">
         <NoticeFeed></NoticeFeed>
-        <div class="flex justify-center flex-wrap gap-10 md:flex-nowrap">
+        <div class="flex justify-center flex-wrap gap-32 md:flex-nowrap">
             <NoticeFeed></NoticeFeed>
             <NoticeFeed></NoticeFeed>
         </div>
-        <NoticeCardSkeleton></NoticeCardSkeleton>
-        <div class="flex flex-col items-center gap-2">
-            <NoticeCard v-for="items in 2" :key="items" :id="items"></NoticeCard>
+        <NoticeCardSkeleton v-if="isLoading"></NoticeCardSkeleton>
+        <div v-else class="flex flex-col items-center gap-2">
+            <div class="min-w-full">
+                <NoticeCard/>
+            </div>
 
             <UPagination v-model="page" :page-count="5" :total="items.length" />
         </div>
@@ -23,6 +25,11 @@ export default {
 
 <script setup lang="ts">
 import {ref} from "vue"
+
+// eslint-disable-next-line no-undef
+
+const isLoading = ref(false)
 const page = ref(1)
 const items = ref(Array(55))
+
 </script>
