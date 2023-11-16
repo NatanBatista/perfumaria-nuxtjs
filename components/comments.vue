ad7ecec253c2619f35f333e7fb58dc161e585cbf
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-11-16 09:10:15 -0300

    style: Criando componente comentarios

---
+++ components/comments.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="border border-b-2 rounded-2xl p-3 w-1/2 m-6">
+            <div class="flex items-center gap-2 mb-2">
+                <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+                <h3 class=""> Natan Batista</h3>
+            </div>
+            <div class="font-extralight tracking-tighter line-clamp-3 lg:line-clamp-none">
+                psum lorem ipsum ipsum lorem ipsum ipsum lorem ipsum ipsum lorem ipsum
+                psum lorem ipsum ipsum lorem ipsum ipsum lorem ipsum ipsum lorem ipsum 
+                psum lorem ipsum ipsum lorem ipsum ipsum lorem ipsum ipsum lorem ipsum. 
+                psum lorem ipsum ipsum lorem ipsum ipsum lorem ipsum ipsum lorem ipsum 
+            </div>
+        </div>
+</template>
+
+<script lang="ts">
+export default {
+	name: "commentsPage"
+}
+</script>
