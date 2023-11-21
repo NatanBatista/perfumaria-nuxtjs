a7b11cccba983c614157e588790827fe71b4b553
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-11-20 21:43:36 -0300

    feat: implementando Sign in

--- components/navbar/signin.vue
+++ components/navbar/signin.vue
@@ -10,16 +10,18 @@
 
         <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
             <UFormGroup label="E-mail" name="email" help="Digite seu email!" required>
-                <UInput v-model="state.email"  icon="i-heroicons-envelope"/>
+                <UInput v-model="state.email" icon="i-heroicons-envelope" />
             </UFormGroup>
 
             <UFormGroup label="Senha" name="password" required>
-                <UInput v-model="state.password" type="password" icon="i-heroicons-key"/>
+                <UInput v-model="state.password" type="password" icon="i-heroicons-key" />
             </UFormGroup>
 
-            <UButton type="submit">
-                Entrar
+            <div class="flex justify-center">
+                <UButton :loading="loading" type="submit">
+                {{ loading ? 'Entrando...' : 'Entrar' }}
             </UButton>
+            </div>
         </UForm>
         <Placeholder class="h-32" />
     </UCard>
@@ -32,11 +34,15 @@ export default {
 </script>
 
 <script setup lang="ts">
+import {ref} from "vue"
 import type { FormSubmitEvent } from "#ui/types"
 import { object, string, type InferType } from "yup"
-import {reactive} from "vue"
+import { reactive } from "vue"
+import axios from "axios"
 
-
+// eslint-disable-next-line no-undef
+const config = useRuntimeConfig()
+const loading = ref(false)
 const schema = object({
 	email: string().email("Email inváldo").required("Obrigatorio"),
 	password: string()
@@ -54,8 +60,21 @@ const state = reactive({
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
 	// Do something with data
-	console.log(event.data)
+	try {
+		loading.value = true
+		const response = await axios.post(config.public.appUrl + "/auth/sign_in", {
+			email: event.data.email,
+			password: event.data.password
+		})
+		document.cookie = `uid=${response.headers["uid"]} path=/`
+		document.cookie = `access-token=${response.headers["access-token"]}`
+		document.cookie = `client=${response.headers["client"]} path=/`
+		
+	} catch (error) {
+		loading.value = false
+		console.error("Erro ao fazer a solicitação POST:", error)
+	} finally {
+		window.location.reload()
+	}
 }
-
-
 </script>
